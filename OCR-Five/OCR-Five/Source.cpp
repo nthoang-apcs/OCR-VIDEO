af8daf86d115dd0c4f8a43305207422b4c66dbd3
vulord <vulord@gmail.com>
2017-08-07 22:45:14 +0700

    Update new version, 2015 vs

--- OCR-Five/OCR-Five/Source.cpp
+++ OCR-Five/OCR-Five/Source.cpp
@@ -6,65 +6,18 @@
 #include <opencv2\imgcodecs.hpp> 
 #include <vector>
 #include <string>
-#include "SupportFunctions.h"
-#include "MyMSER.h"
-#include "CannyAlgorithms.h"
-#include "BackGround.h"
-#include "GaussianBlur.h"
-#include "HarrisDetector.h"
-#include "AssignmentTwo.h"
 
 using namespace std;
 using namespace cv;
 using namespace cv::text;
 
 
-/*			Note		*/
-// all algorithms have named 'My' before their filenames
 
 int main(char* argv)
 {
-	//BackGround *a = new BackGround();
-	//a->Run();
-	int ny = 10;
-	int nx = 20;
-	for (int m = 0; m < nx; m++)
-	{
-		for (int n = 0; n < ny; n++)
-		{
-			int c = m + nx * n;
-			cout << c << " ";
-		}
-		cout << endl;
-	}
+	Mat image = imread("E:\\Code\\OCR-Five-Git\\OCR-Five\\OCR-Five\\testcanny-1.jpg");
+	imshow("canny", image);
+
 	return 0;
 }
 
-
-// default ocr in open cv
-/*
-char srcVietDir[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentUse\\Vietnamese";
-char srcEngDir[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentUse\\English";
-int srcVietCount = 60;
-int srcEngCount = 350;
-char destResultViet[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\Vietnamese";
-char destResultEng[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\English";
-char errorPath[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\error-log.txt";
-char NM1[] = "D:\\document\\LVTN2016\\OCRThree\\OCRThree\\trained_classifierNM1.xml";
-char NM2[] = "D:\\document\\LVTN2016\\OCRThree\\OCRThree\\trained_classifierNM2.xml";
-char groupboxesVietFilePath[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\Vietnamese-data.txt";
-char groupboxesEngFilePath[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\English-data.txt";
-
-TDTCNM *a = new TDTCNM(srcVietDir, srcEngDir, srcVietCount, srcEngCount, NM1, NM2, destResultViet, destResultEng, errorPath, groupboxesVietFilePath, groupboxesEngFilePath);
-a->RunForVietNameseSample(false);
-a->RunForEnglishSample(false);
-*/
-
-// Get statistic
-/*
-char englishDataPath[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\English-data.txt";
-char vietnameseDataPath[] = "D:\\document\\LVTN2016\\DataOCR\\CurrentResult-DefaultInOCR\\Vietnamese-data.txt";
-vector<string>List;
-SupportFunctions* spfunc = new SupportFunctions();
-spfunc->ReadDataGroupBoxesFromFileText(List, vietnameseDataPath);
-*/
